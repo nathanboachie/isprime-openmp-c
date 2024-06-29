109b9f84da4c6a4550ccd80478e85e9a151d61b0
nathanboachie <124912901+nathanboachie@users.noreply.github.com>
2024-06-29 12:48:50 +0100

    Add files via upload

--- src/prime.c
+++ src/prime.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
@@ -5,34 +6,44 @@
 
 int main()
 {
-	double start = omp_get_wtime();
-	int MAX = 200000000;
-	int total = 2;
-	int j = 0;
-	#pragma omp parallel default(none) shared(MAX,total) private(j)
-	{
-	int local_total = 0;
-	#pragma omp for schedule(guided,700)  
-	for(int i=3; i<MAX ; i++)
-	{
-		int sqrt_i = (int)sqrt(i);
-		j = 2;
-		while(j < sqrt_i && (i%j)!= 0)
-		{
-			j+=1;
-		}
-		if(j < sqrt_i)
-		{
-			continue;
-		}
-		else
-		{
-			local_total++;
-		}
-	}
-	#pragma omp atomic
-	total += local_total;
-	}
-	printf("Finding all prime numbers under %d took %.2f seconds and %d total primes found\n",MAX,omp_get_wtime()-start,total); 
-	return 0;
+    int MAX;
+    // Prompt user to enter MAX value
+    printf("Enter a value for MAX (greater than 2): ");
+    scanf("%d", &MAX);
+
+    if (MAX <= 2) {
+        printf("Invalid input. Please enter a value greater than 2.\n");
+        return 1;
+    }
+
+    double start = omp_get_wtime();
+    int total = 2;
+    int j = 0;
+    #pragma omp parallel default(none) shared(MAX,total) private(j)
+    {
+        int local_total = 0;
+        #pragma omp for schedule(guided, 700)
+        for(int i=3; i<MAX ; i++)
+        {
+            int sqrt_i = (int)sqrt(i);
+            j = 2;
+            while(j < sqrt_i && (i % j) != 0)
+            {
+                j += 1;
+            }
+            if(j < sqrt_i)
+            {
+                continue;
+            }
+            else
+            {
+                local_total++;
+            }
+        }
+        #pragma omp atomic
+        total += local_total;
+    }
+
+    printf("Finding all prime numbers under %d took %.2f seconds and %d total primes found\n", MAX, omp_get_wtime() - start, total);
+    return 0;
 }
