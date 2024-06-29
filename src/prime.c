5a8189b03b92cb0039d59bbf6ef098c97cd05300
Nathan Boachie <nathanboachie@outlook.com>
2024-06-29 12:23:10 +0100

    Optimised code

--- src/prime.c
+++ src/prime.c
@@ -4,33 +4,34 @@
 #include <math.h>
 
 int main()
-{	
+{
 	double start = omp_get_wtime();
-	
 	int MAX = 200000000;
-	int total = 1;
+	int total = 2;
 	int j = 0;
-	int chunk_size = MAX/72;
 	#pragma omp parallel default(none) shared(MAX,total) private(j)
 	{
-	#pragma omp for reduction(+:total) schedule(guided,2777778)  
-	for(int i=2; i<MAX ; ++i)
+	int local_total = 0;
+	#pragma omp for schedule(guided,700)  
+	for(int i=3; i<MAX ; i++)
 	{
+		int sqrt_i = (int)sqrt(i);
 		j = 2;
-		while(j < (int)sqrt(i) && (i%j)!= 0)
+		while(j < sqrt_i && (i%j)!= 0)
 		{
 			j+=1;
 		}
-		if(j < (int)sqrt(i))
+		if(j < sqrt_i)
 		{
 			continue;
 		}
-
 		else
 		{
-			total+=1;
+			local_total++;
 		}
 	}
+	#pragma omp atomic
+	total += local_total;
 	}
 	printf("Finding all prime numbers under %d took %.2f seconds and %d total primes found\n",MAX,omp_get_wtime()-start,total); 
 	return 0;
